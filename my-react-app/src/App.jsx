5ebc21632abf200c269658f4764144bad8ee168f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2025-02-16 17:30:50 +0000

    Create Button component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
-import Card from './Card.jsx';
+// import Card from './Card.jsx';
+import Button from './Button.jsx';
 
 function App() {
   return (
     <>
       <Header />
-      <Card />
-      <Card />
+      <Button />
       <Footer />
     </>
   );
