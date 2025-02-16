e4ac14259802b33d8156ab5dc865e3b7df0e5c9e
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2025-02-16 17:47:11 +0000

    Demonstrate modules to style Button componen

--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -1,8 +1,8 @@
-function Button() {
-    return(
-        <button className="button">Click me</button>
-    );
+import styles from "./Button.module.css";
 
+function Button() {
+  return( <button className={styles.button}>Click me</button>
+  );
 }
 
 export default Button;
