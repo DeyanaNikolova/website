33cf7f82db185af062a9d78ada554f024f13e06d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2025-02-16 17:13:14 +0000

    Import Card component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,13 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Card from './Card.jsx';
 
 function App() {
   return (
     <>
       <Header />
-      <Food/>
+      <Card />
+      <Card />
       <Footer />
     </>
   );
